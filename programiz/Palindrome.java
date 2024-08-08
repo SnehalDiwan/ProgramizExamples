b45f7d3e255160385090c1f64ca0debe31208ba9
SnehalDiwan <snehalkulkarni900@gmail.com>
2024-08-08 11:57:08 +0530

    thrid commit

--- programiz/Palindrome.java
+++ programiz/Palindrome.java
@@ -4,5 +4,6 @@ public class Palindrome {
     public static void main(String[] args) {
         System.out.println("Palindrome program");
         System.out.println("second commit");
+        System.out.println("third commit");
     }
 }
